b176fc0da566b590c89e90add9920975e87868ae
Pedro Tosta <pedrotosta11@gmail.com>
2023-12-18 10:05:29 -0400

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const url = "tads4spring-production.up.railway.app/";
+
+function Products(){
+  return(
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function Users(){
+  return(
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.txt}>Olá mundo!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function Telas(){
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="Users" component={Users} 
+        options={{
+          tabBarLabel: 'Users',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="account-circle" size={size} color={color} />;
+          },
+        }}/>
+      <Tab.Screen name="Products" component={Products} />
+    </Tab.Navigator>
+  )
+}
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Telas/>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    marginTop: 30
   },
+  box:{    
+    width: '90%',
+    backgroundColor: '#f6f6f6',
+    height: 150,
+    alignItems: 'center',
+    elevation: 5,
+  },
+  txt:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 10
+  }
 });
+
+export default App;
